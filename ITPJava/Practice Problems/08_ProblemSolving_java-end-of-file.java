f2d635954169854c839df0bbc98873e2f82459f4
Nusaiba <57018533+smeraldoflower@users.noreply.github.com>
2020-10-03 19:35:40 -0500

    Update 08_ProblemSolving_java-end-of-file.java

--- ITPJava/Practice Problems/08_ProblemSolving_java-end-of-file.java
+++ ITPJava/Practice Problems/08_ProblemSolving_java-end-of-file.java
@@ -7,7 +7,7 @@ public class Solution
 {
     public static void main(String[] args)
     {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your                class should be named Solution. */
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
 
         Scanner scan = new Scanner(System.in);
         long i = 1;
