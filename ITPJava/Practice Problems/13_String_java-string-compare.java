a4d45b5b3cd82676405ee3e41a2c89dc84ff58b6
Nusaiba <57018533+smeraldoflower@users.noreply.github.com>
2020-10-03 19:53:07 -0500

    Create 13_String_java-string-compare.java

---
+++ ITPJava/Practice Problems/13_String_java-string-compare.java
@@ -0,0 +1,49 @@
+// PROBLEM: https://www.hackerrank.com/challenges/java-string-compare/problem
+
+import java.util.Scanner;
+
+public class Solution {
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static String getSmallestAndLargest(String s, int k)
+    {
+        String smallest = "";
+        String largest = "";
+
+        String substr = s.substring(0,k);
+        smallest = s.substring(0,k);
+        largest = s.substring(0,k);
+
+        for(int i=0; i<=(s.length()-k); i++)
+        {
+            substr = s.substring(i,(i+k));
+
+            if(substr.compareTo(smallest)<0)
+            {
+                smallest = substr;
+            }
+            if(substr.compareTo(largest)>0)
+            {
+                largest = substr;
+            }            
+        }
+
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        
+        return smallest + "\n" + largest;
+    }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
