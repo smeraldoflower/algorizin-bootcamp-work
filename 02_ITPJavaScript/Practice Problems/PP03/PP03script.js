5cc1b364a3a3289337ecc604081c64c25d83b09d
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2020-12-01 16:44:17 -0600

    added pp03 files

---
+++ 02_ITPJavaScript/Practice Problems/PP03/PP03script.js
@@ -0,0 +1,37 @@
+// Nusaiba Mahmood
+// ITP JS HW 3
+// HW03script.js
+
+function getFormInput()
+{
+    let name = document.getElementById("student_form").elements.namedItem("student_name").value;
+    let age = document.getElementById("student_form").elements.namedItem("age").value;
+    let phone = document.getElementById("student_form").elements.namedItem("phone").value;
+    let address = document.getElementById("student_form").elements.namedItem("address").value;
+
+    console.log(name);
+    console.log(age);
+    console.log(phone);
+    console.log(address);
+
+    document.getElementById("table-headings").insertAdjacentHTML
+    (
+        'afterend',
+        `<tr>
+            <td>${name}</td>
+            <td>${age}</td>
+            <td>${phone}</td>
+            <td>${address}</td>
+            <td><button id="deleteButton">DELETE</button></td>
+        </tr>`
+    );
+    
+    //TO DO: Add functionality to the delete button
+    
+    //document.getElementById("deleteButton").addEventListener("click",)
+};
+
+document
+    //.getElementById("student_form").elements.namedItem("addButton")
+    .getElementById("addButton")
+    .addEventListener("click", getFormInput);
